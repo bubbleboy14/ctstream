c6c36247b536287321e7a20e20e14cf2f809d13d
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-20 22:41:32 -0800

    changed default markup titles. init: create html/stream; copy html pages (instead of symming directory)

--- ctstream/init.py
+++ ctstream/init.py
@@ -1,10 +1,14 @@
+import os
+hsdir = os.path.join("html", "stream")
+dirs = [hsdir]
+
 copies = {
 	"img": ["audio.png", "remote_control.png", "save.png", "tv.png"],
 	"css": ["custom.css"]
 }
+copies[hsdir] = ["index.html", "widget.html"]
 
 syms = {
-	"html": ["stream"], # copy these?
 	"css": ["stream.css"],
 	"js": ["stream"]
 }
