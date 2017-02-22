c00d03b5a728bb302bb0948e8eba649c1922d165
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-22 13:06:38 -0800

    updated config w/ new default options: no_title, video_opts, chatblurs

--- ctstream/js/config.js
+++ ctstream/js/config.js
@@ -3,5 +3,8 @@
 	"redirect": "/",
 	"storage_key": "ssk",
 	"port": 8888,
-	"channels": []
+	"channels": [],
+	"no_title": false,
+	"video_opts": {},
+	"chatblurs": ["say what?", "any questions?", "what's up?"]
 }
