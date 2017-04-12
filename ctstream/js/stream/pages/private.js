065f21db22a6062d88d4dce918a6823910e960ca
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-12 04:06:28 -0700

    private pw checking

--- ctstream/js/stream/pages/private.js
+++ ctstream/js/stream/pages/private.js
@@ -1,9 +1,20 @@
-CT.require("CT.dom");
-CT.require("CT.storage");
+CT.require("CT.all");
 CT.require("core");
 
-var cfg = JSON.parse(atob(unescape(location.hash.slice(1))));
-cfg.chat = true;
-cfg.lurk = true;
-CT.storage.set(core.config.ctstream.storage_key, cfg);
-location = "/stream";
+CT.onload(function() {
+	(new CT.modal.Prompt({
+		prompt: "what's the password?",
+		cb: function(val) {
+			CT.memcache.countdown.get(val, function(show) {
+				var cfg = JSON.parse(atob(unescape(location.hash.slice(1))));
+				cfg.chat = true;
+				cfg.lurk = true;
+				if (show && show.token == cfg.channel) {
+					CT.storage.set(core.config.ctstream.storage_key, cfg);
+					location = "/stream";
+				} else
+					location = core.config.ctstream.redirect;
+			});
+		}
+	})).show();
+});
