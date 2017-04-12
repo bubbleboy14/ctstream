43bc2bbe8580da02efb74fee2a46b2806bba2338
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-11 21:56:21 -0700

    init: updated syms{}, routes{} (_pw->_stream); added streamails to copies map

--- ctstream/init.py
+++ ctstream/init.py
@@ -3,17 +3,18 @@ hsdir = os.path.join("html", "stream")
 dirs = [hsdir]
 
 copies = {
+	".": ["streamails.py"],
 	"img": ["audio.png", "remote_control.png", "save.png", "tv.png"],
 	"css": ["custom.css"]
 }
 copies[hsdir] = ["countdown.html", "index.html", "schedule.html", "widget.html"]
 
 syms = {
-	".": ["_pw.py"],
+	".": ["_stream.py"],
 	"js": ["stream"],
 	"css": ["stream.css"]
 }
 
 routes = {
-	"/_pw": "_pw.py"
+	"/_stream": "_stream.py"
 }
