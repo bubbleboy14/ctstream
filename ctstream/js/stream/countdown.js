2f11a2477df167a18cd83b80e7bb14211cea541e
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-20 17:56:28 -0700

    countdown: prettified, rounded, localized timestamps

--- ctstream/js/stream/countdown.js
+++ ctstream/js/stream/countdown.js
@@ -18,11 +18,17 @@ stream.countdown = {
 			})
 		];
 	},
+	_timestamp: function(ttl) {
+		var d = new Date(Math.round((Date.now() + ttl * 1000) / 1800000) * 1800000);
+		return d.toString().split(d.getFullYear())[0] + "@ " + d.toLocaleTimeString().replace(":00 ", " ");
+	},
 	node: function(show) {
 		return show ? (show.ttl > 0 ? [
 				CT.dom.span(core.config.ctstream.copy.countdown),
 				CT.dom.pad(),
-				CT.parse.countdown(show.ttl)
+				CT.parse.countdown(show.ttl),
+				CT.dom.pad(),
+				CT.dom.span("(your time: " + stream.countdown._timestamp(show.ttl) + ")")
 			] : stream.countdown._show_or_no(show)) : core.config.ctstream.copy.nothing;
 	},
 	load: function(show, node) {
