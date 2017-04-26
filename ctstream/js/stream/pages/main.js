6f439187b696fa29635be540a2c8459a3b5fe67d
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-25 18:26:42 -0700

    stream page, beforeunload: only clear storage if cfg.require_admin and no admin user (keeps tricky lurkers out w/o inconveniencing hosts

--- ctstream/js/stream/pages/main.js
+++ ctstream/js/stream/pages/main.js
@@ -4,19 +4,22 @@ CT.require("core");
 CT.require("stream.core");
 CT.pubsub.set_protocol("wss");
 CT.pubsub.set_reconnect(false);
-if (core.config.ctstream.transcode)
+var cfg = core.config.ctstream;
+if (cfg.transcode)
 	CT.stream.opts.setTranscoder(stream.core.transcode);
 
 CT.onload(function() {
-	for (var o in core.config.ctstream.stream_opts)
-		CT.stream.opts[o] = core.config.ctstream.stream_opts[o];
+	for (var o in cfg.stream_opts)
+		CT.stream.opts[o] = cfg.stream_opts[o];
 	stream.core.setNode(CT.dom.id("ctmain"));
 	if (core.config.footer) {
-		core.config.ctstream.channels.length &&
-			stream.core.loadChannels(core.config.ctstream.channels);
+		cfg.channels.length && stream.core.loadChannels(cfg.channels);
 		stream.core.loadRemote();
 	}
 	CT.initCore();
 	stream.core.init();
-	CT.on("beforeunload", CT.storage.clear);
+	CT.on("beforeunload", function() {
+		if (cfg.require_admin && !(user && user.core._current && user.core._current.admin))
+			CT.storage.clear();
+	});
 });
