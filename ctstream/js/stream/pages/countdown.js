cacf69fc81c9161d76996797946b94de551a2886
bubbleboy14 <mario.balibrera@gmail.com>
2017-05-15 03:00:39 -0700

    countdown redirect: special-case localhost to avoid configuring live/local frontends differently

--- ctstream/js/stream/pages/countdown.js
+++ ctstream/js/stream/pages/countdown.js
@@ -5,6 +5,7 @@ CT.require("stream.core");
 var cfg = core.config.ctstream,
 	cph = cfg.countdown_parent_host; 
 if (cph && cfg.redirect &&
+	location.hostname.indexOf("localhost") == -1 &&
 	document.referrer.indexOf(cph) == -1)
 		window.open(cfg.redirect, "_top");
 
