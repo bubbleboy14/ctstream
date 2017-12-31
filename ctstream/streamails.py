25b84a310a4dcdbc8361238a4c2b889908f741dd
bubbleboy14 <mario.balibrera@gmail.com>
2017-12-31 01:34:10 -0800

    private_meeting email template, and privates{} object w/ other templates

--- ctstream/streamails.py
+++ ctstream/streamails.py
@@ -2,3 +2,11 @@ private_show = {
 	"subject": "You've been invited to a private show",
 	"body": "It starts on %s. To watch, click <a href=\"%s\">here</a> and enter this password: '%s'. Enjoy!"
 }
+private_meeting = {
+	"subject": "You've been invited to a private meeting",
+	"body": "It starts on %s. To join, click <a href=\"%s\">here</a> and enter this password: '%s'. Enjoy!"
+}
+privates = {
+	"show": private_show,
+	"meeting": private_meeting
+}
