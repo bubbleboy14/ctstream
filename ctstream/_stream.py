3cdefd926d02298e51dd0b4cece47a61dbf2a4b9
bubbleboy14 <mario.balibrera@gmail.com>
2017-12-31 01:35:18 -0800

    /_stream hander: "invite" action triggers invite() (instead of doing the same thing)

--- ctstream/_stream.py
+++ ctstream/_stream.py
@@ -1,8 +1,8 @@
-import base64, json, time, urllib
-from cantools.web import respond, fail, read_file, send_file, send_mail, cgi_get, getmem, clearmem
+from cantools.web import respond, fail, read_file, send_file, cgi_get, clearmem
 from cantools.util import transcode
 from cantools import config
-from streamails import private_show
+from ctstream import invite
+from streamails import privates
 
 def response():
 	if cgi_get("pw") != config.pw:
@@ -13,19 +13,6 @@ def response():
 	elif action == "transcode":
 		send_file(transcode(read_file(cgi_get("data")), True))
 	elif action == "invite":
-		pw = cgi_get("show")
-		msg = cgi_get("msg", required=False)
-		show = getmem(pw, False)
-		ts = "%s %s"%(show["ttl"].strftime("%A at %-I:%M%p"), time.tzname[time.daylight])
-		baseaddr = "%s://%s/stream/private.html#"%(config.web.protocol, config.web.domain)
-		for email in cgi_get("emails"):
-			credz = urllib.quote(base64.b64encode(json.dumps({
-				"channel": show["token"],
-				"user": email.split("@")[0]
-			})))
-			link = "%s%s"%(baseaddr, credz)
-			body = msg and msg.format(link='<a href="%s">here</a>'%(link,), password=pw,
-				time=ts) or private_show["body"]%(ts, link, pw)
-			send_mail(email, subject=private_show["subject"], body=body)
+		invite(cgi_get("show"), cgi_get("emails"), cgi_get("msg", required=False), privates[cgi_get("stream_type", default="show")])
 
 respond(response)
