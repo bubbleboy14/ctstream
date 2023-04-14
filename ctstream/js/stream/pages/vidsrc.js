5fcd911ddf75b5be9870939368c1da8f9f9f70f5
bubbleboy14 <mario.balibrera@gmail.com>
2023-04-14 13:00:55 -0700

    vidsrc page loader: extracted procBuff() from blob_to_buffer() cb(); ignore own video errors; support stream action (via stream.core.startRecord(stream.core.multiplex()) with procBuff() onbuff()

--- ctstream/js/stream/pages/vidsrc.js
+++ ctstream/js/stream/pages/vidsrc.js
@@ -3,7 +3,15 @@ CT.require("core");
 CT.pubsub.set_protocol("wss");
 
 CT.onload(function() {
-	var targetOrigin, chans = {}, senders = {},
+	var procBuff = function(buffer, channel) {
+		window.parent.postMessage({
+			action: "clip",
+			data: {
+				channel: channel,
+				data: buffer
+			}
+		}, targetOrigin);
+	}, targetOrigin, chans = {}, senders = {},
 		user = "embedded" + CT.data.random(100000);
 	CT.pubsub.connect(location.hostname, core.config.ctstream.port, user);
 	window.addEventListener("message", function(evt) {
@@ -14,6 +22,8 @@ CT.onload(function() {
 				chans[d.data] = { requiredInitChunk: "unset" };
 				CT.pubsub.subscribe(d.data);
 			} else if (d.action == "error") {
+				if (!(d.data.channel in chans))
+					return CT.log("error on own video ignored");
 				chans[d.data.channel].requiredInitChunk = d.requiredInitChunk;
 				CT.pubsub.publish(d.data.channel, {
 					action: "error",
@@ -22,6 +32,10 @@ CT.onload(function() {
 						sender: senders[d.data.channel]
 					}
 				});
+			} else if (d.action == "stream") {
+				CT.require("stream.core", true);
+				stream.core.startRecord(stream.core.multiplex(d.data,
+					false, false, true, buffer => procBuff(buffer, d.data)));
 			}
 		}
 	});
@@ -32,15 +46,7 @@ CT.onload(function() {
 			if (cdata.requiredInitChunk == "unset")
 				cdata.requiredInitChunk = data.channel + data.user + "init";
 			CT.stream.util.update(data.message.data, function(blob) {
-				CT.stream.util.blob_to_buffer(blob, function(buffer) {
-					window.parent.postMessage({
-						action: "clip",
-						data: {
-							channel: data.channel,
-							data: buffer
-						}
-					}, targetOrigin);
-				});
+				CT.stream.util.blob_to_buffer(blob, buffer => procBuff(buffer, data.channel));
 			}, cdata.requiredInitChunk);
 			delete cdata.requiredInitChunk;
 		}
